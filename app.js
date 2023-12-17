031c09a7d9a6d442b3d55998e8ae5555521919bf
221y051 <221y051@epson-isc.com>
2023-12-17 18:11:05 +0900

    app commit

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(session(session_opt));
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-app.set('views', __dirname + '/views'); 
+app.set('views', __dirname + '/views'); //12月１７日午後４時４４分に修正
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
