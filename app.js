488ec2f06c3e7b7e6c1c6a023822a5e7f8bb8e96
221y051 <221y051@epson-isc.com>
2023-12-24 13:13:09 +0900

    app commit

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var helloRouter = require('./routes/hello');
 
 var boardsRouter = require('./routes/boards');
-const helmet = require('helmet');
+
 
 var createError = require('http-errors');
 var express = require('express');
@@ -27,6 +27,7 @@ app.use(session(session_opt));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 //app.set('views', __dirname + '/views'); //12月１７日午後４時４４分に修正
+app.set("views", appRoot.resolve("ex-gen-app/views"));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -36,8 +37,7 @@ app.use('/boards', boardsRouter);
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/hello', helloRouter);
-app.use(helmet());
-app.disable('x-powered-by')
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
